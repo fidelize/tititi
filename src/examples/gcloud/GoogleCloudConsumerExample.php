584148e1a0f3a30822208e2dfd5c655fac6128a9
Gleydson Silva <gleydson.silva@fidelize.com.br>
2019-06-27 11:28:43 -0300

    Adição do exemplo do google cloud

--- src/examples/gcloud/GoogleCloudConsumerExample.php
+++ src/examples/gcloud/GoogleCloudConsumerExample.php
@@ -2,14 +2,14 @@
 
 include __DIR__ . '/../../../vendor/autoload.php';
 
-putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/../your-gcloud-key.json');
+putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/../../../data/baratona-d15119261d3d.json');
 
 $client = new \Google\Cloud\PubSub\PubSubClient([
-    'projectId' => 'your-project-id-here',
+    'projectId' => 'baratona',
 ]);
 
 $adapter = new \Superbalist\PubSub\GoogleCloud\GoogleCloudPubSubAdapter($client);
 
-$adapter->subscribe('my_channel', function ($message) {
+$adapter->subscribe('PBM_SANOFI', function ($message) {
     var_dump($message);
 });
--- src/examples/gcloud/GoogleCloudPublishExample.php
+++ src/examples/gcloud/GoogleCloudPublishExample.php
@@ -2,14 +2,14 @@
 
 include __DIR__ . '/../../../vendor/autoload.php';
 
-putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/../your-gcloud-key.json');
+putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/../../../data/baratona-d15119261d3d.json');
 
 $client = new \Google\Cloud\PubSub\PubSubClient([
-    'projectId' => 'your-project-id-here',
+    'projectId' => 'baratona',
 ]);
 
 $adapter = new \Superbalist\PubSub\GoogleCloud\GoogleCloudPubSubAdapter($client);
 
-$adapter->publish('my_channel', 'Hello World');
-$adapter->publish('my_channel', ['lorem' => 'ipsum']);
-$adapter->publish('my_channel', '{"blah": "bleh"}');
+$adapter->publish('PBM_SANOFI', 'Hello World');
+$adapter->publish('PBM_SANOFI', ['lorem' => 'ipsum']);
+$adapter->publish('PBM_SANOFI', '{"blah": "bleh"}');
